50354bb8d72144786ca1686c1067246157fb14da
affanhasby <hasbyaffan@gmail.com>
2021-02-17 11:43:22 +0700

    versi php7

--- application/config/database.php
+++ application/config/database.php
@@ -26,8 +26,8 @@
 |	['cachedir'] The path to the folder where cache files should be stored
 |	['char_set'] The character set used in communicating with the database
 |	['dbcollat'] The character collation used in communicating with the database
-|				 NOTE: For MySQL and MySQLi databases, this setting is only used
-| 				 as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
+|				 NOTE: For mysql and MySQLi databases, this setting is only used
+| 				 as a backup if your server is running PHP < 5.2.3 or mysql < 5.0.7
 |				 (and in table creation queries made with DB Forge).
 | 				 There is an incompatibility in PHP with mysql_real_escape_string() which
 | 				 can make your site vulnerable to SQL injection if you are using a
